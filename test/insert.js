3f4793b97c4927847a5726ff8bc27c2b8fd08966
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-02-02 14:20:10 +0100

    Insert didnt return expected data structure.
    It should return the same structure as passed in.

--- test/insert.js
+++ test/insert.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Insert = require('../index').Insert;
-const _ = require('lodash');
 const Lab = require('lab');
 const Code = require('code');
 const expect = Code.expect;
@@ -20,6 +19,11 @@ const fixture = {
         {
             name: 'Item 2'
         }
+    ],
+    artists: [
+        {
+            name: 'Crazy Ass Wicked Wild'
+        }
     ]
 };
 
@@ -27,13 +31,14 @@ lab.experiment('Insert', () => {
 
     lab.test('it inserts', (done) => {
 
-        Insert(options, fixture).then( (result) => {
+        Insert(options, fixture).then( (objects) => {
 
-            const names = result.items.map( (item) => {
+            const idx = objects.items.findIndex( (item) => {
 
-                return item.name;
+                return item.name === 'Item 1';
             });
-            expect(_.includes(names, 'Item 1')).to.be.equal(true);
+            expect(objects.items[idx].name).to.be.equal(fixture.items[0].name);
+            expect(objects.artists[0].name).to.be.equal(fixture.artists[0].name);
             done();
         }, console.error);
     });
