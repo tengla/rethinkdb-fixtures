42c0be52d11ecbb84cdbcd79f8f7e40400f8fdf3
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-06-06 01:35:26 +0200

    Json file loading - globbing
    Load json files by pattern.

---
+++ test/readall.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const Readall = require('../index').all;
+const Lab = require('lab');
+const Code = require('code');
+const expect = Code.expect;
+const lab = exports.lab = Lab.script();
+
+lab.experiment('Readall', () => {
+
+    lab.test('it reads all fixtures', (done) => {
+
+        Readall('./fixtures/*json')
+        .then( (json) => {
+
+            expect(json.bikes).to.be.array();
+            done();
+        })
+        .catch( (err) => {
+
+            done(err);
+        });
+    });
+
+    lab.test('it fails to read', (done) => {
+
+        Readall('./boguspath/*json')
+        .then( () => {})
+        .catch( (err) => {
+
+            expect(err.message).to.be.equal('Cannot convert undefined or null to object');
+            done();
+        });
+    });
+});
