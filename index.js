51ef28d1190329a60c3c5ff48e3be9f1c6824c36
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-06-06 01:38:43 +0200

    export 'readall' module

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 module.exports.Prepare = require('./lib/prepare');
 module.exports.Readfixture = require('./lib/readfixture');
+module.exports.all = require('./lib/readall');
 module.exports.Insert = require('./lib/insert');
 module.exports.Delete = require('./lib/delete');
