a043cb5b709afb51cdf2a977d6dc7495f82025a6
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-06-10 00:54:42 +0200

    Prepare renamed to base

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-module.exports.Prepare = require('./lib/prepare');
 module.exports.Readfixture = require('./lib/readfixture');
 module.exports.all = require('./lib/readall');
+module.exports.Base = require('./lib/base');
 module.exports.Insert = require('./lib/insert');
 module.exports.Delete = require('./lib/delete');
