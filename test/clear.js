5d3f3e0fc51dd6966e1cf49d009746ab6661fc07
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-09-19 23:28:58 +0200

    Remove before and after functions ..
    and make more elaborate test case.

--- test/clear.js
+++ test/clear.js
@@ -1,19 +1,17 @@
 'use strict';
 
 const R = require('rethinkdb');
-
-const Delete = require('../index').Delete;
 const Insert = require('../index').Insert;
 const Lab = require('lab');
 const Code = require('code');
 const expect = Code.expect;
-let conn;
 const lab = exports.lab = Lab.script();
 
 const options = {
     host: process.env.RETHINKDB_INSTANCE_PORT_8080_TCP_ADDR,
     db: process.env.RETHINKDB || 'test',
-    verbose: 1,
+    user: 'test',
+    verbose: true,
     clear: true
 };
 
@@ -38,58 +36,37 @@ const fixture = {
 
 lab.experiment('Clear', () => {
 
-    lab.before( (done) => {
-
-        // We need to peek into db to make this test
-        R.connect({ db: options.db, host: options.host }).then( (_conn) => {
-
-            conn = _conn;
-            done();
-        }, done);
-    });
-
-    lab.before( (done) => {
-
-        Insert(options,fixture).then( (result) => {
-
-            done();
-        }, done);
-    });
-
-    lab.after( (done) => {
-
-        Delete(options, ['items', 'weirdos']).then( (result) => {
-
-            done();
-        }, done);
-    });
-
-    lab.after( (done) => {
-
-        conn.close();
-        done();
-    });
-
     lab.test('it clears table before inserting', (done) => {
-
-        fixture.items.push({
-            name: 'Third item'
-        });
 
         Insert(options, fixture)
         .then( (created) => {
 
-            return R.table('items').count().run(conn);
-        }).then( (count) => {
+            return created;
+        })
+        .then( () => {
 
-            // The two items from 'before' was called should have been deleted,
-            // and three items from this run should have been inserted.
-            expect(count).to.be.equal(3);
+            return Insert(options, fixture);
+        })
+        .then( () => {
+
+            return R.connect(options);
+        })
+        .then( (conn) => {
+
+            return Promise.all([R.table('items').run(conn), conn]);
+        })
+        .then( (promises) => {
+
+            const [cursor, conn] = promises;
+            return Promise.all([cursor.toArray(), conn]);
+        })
+        .then( (promises) => {
+
+            const [items,conn] = promises;
+            expect(items.length).to.equal(2);
+            conn.close();
             done();
         })
-        .catch( (err) => {
-
-            done(err);
-        });
+        .catch(done);
     });
 });
