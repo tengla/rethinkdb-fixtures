75b37eb990f5c036cf7693ecac34d83f5721b3d5
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-10-11 00:57:15 +0200

    Update: let connection be reused.
    It was very slow to open a cnx everytime
    I wanted to use Insert.
    
    Now: faster than a pussycat.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 
-module.exports.Readfixture = require('./lib/readfixture');
-module.exports.all = require('./lib/readall');
-module.exports.Base = require('./lib/base');
-module.exports.Insert = require('./lib/insert');
-module.exports.Delete = require('./lib/delete');
+const Insert = require('./insert');
+const Delete = require('./delete');
+const Base = require('./base');
+
+module.exports = function (config) {
+
+    const base = new Base(config);
+    
+    return {
+        Insert: Insert(undefined,base),
+        Delete: Delete(undefined,base)
+    }
+};
